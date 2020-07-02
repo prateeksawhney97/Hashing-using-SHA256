eb8424a55e76f38b6d5a4057427668c9e044d641
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2020-07-02 12:49:41 +0530

    retrieving the hash

--- app.js
+++ app.js
@@ -8,7 +8,8 @@
  */
 
 // Write your code here
-
+//import sha256 from 'crypto-js/sha256';
+var SHA256 = require("crypto-js/sha256");
 /**
  * Variables: Do not change variable values to be able to answer the quiz
  */
@@ -28,6 +29,7 @@ const dataObject = {
 
 function generateHash(obj) {
 	// Write your code here
+	return SHA256(JSON.stringify(obj));
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
